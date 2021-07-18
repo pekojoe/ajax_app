c8e02ad0d83ca26cfe4f9b14850edc2eba1700fa
pekojoe <85988618+pekojoe@users.noreply.github.com>
2021-07-18 15:59:16 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,18 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post; //レスポンスの中から投稿されたメモの情報を抽出し、変数itemに格納
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;  //item内に格納されたメモの情報を元にして、ブラウザに描画するためのHTMLを生成し、変数htmlに格納
+  return html;
+};
+
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -8,6 +23,16 @@ function post (){
     XHR.open("POST", "/posts", true); //リクエストの内容をopen()メソッドで指定
     XHR.responseType = "json"; //サーバーからのレスポンスの形式をJSONに指定
     XHR.send(formData); //フォームに入力された内容をsend()メソッドでサーバー側に送信
+    XHR.onload = () => {  //onloadプロパティで、リクエストの送信に成功したときに行う処理を定義
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const list = document.getElementById("list"); //新しいメモを挿入するための要素を取得して、変数listに格納
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR)); //変数listに格納された要素の直後に生成したHTMLを挿入
+      formText.value = "";
+    };
   });
 };
 
