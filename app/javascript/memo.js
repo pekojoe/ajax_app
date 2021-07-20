c4559a116ce82feca583318da7609de3bbcabcdb
pekojoe <85988618+pekojoe@users.noreply.github.com>
2021-07-20 13:58:29 +0900

    credentials.yml.encの作成

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -18,7 +18,7 @@ function post (){
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
-    const formData = new FormData(form); //FormDataオブジェクトを使って、フォームの値を取得
+    const formData = new FormData(form); //FormDataオブジェクトを使って、フォームの値を取得し、Ajaxで送信できる形へと整形
     const XHR = new XMLHttpRequest(); //非同期通信を行うためにXMLHttpRequestオブジェクトを生成
     XHR.open("POST", "/posts", true); //リクエストの内容をopen()メソッドで指定
     XHR.responseType = "json"; //サーバーからのレスポンスの形式をJSONに指定
